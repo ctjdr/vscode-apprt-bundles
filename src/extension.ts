0ff94d1d22ca061bac04fc29ce79b5a65a74eb95
Jan Drewnak <j.drewnak@conterra.de>
2020-09-24 22:50:52 +0200

    Cleanup extension bootstrap code

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,3 @@
-import { resolve } from 'path';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import ManifestDocument, { DocumentElement } from "./ManifestDocument";
 
@@ -10,33 +7,14 @@ const manifestFilesSelector: vscode.DocumentSelector = {
     pattern: "**/manifest.json"
 };
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-manifest-navigation" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('vscode-manifest-navigation.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from vscode-manifest-navigation!');
-    });
-
-
+  
     context.subscriptions.push(
         vscode.languages.registerReferenceProvider(
             manifestFilesSelector, new BundleReferenceProvider()));
 
 
-
-
-    context.subscriptions.push(disposable);
 }
 
 
@@ -90,5 +68,4 @@ class BundleReferenceProvider implements vscode.ReferenceProvider {
     }
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
