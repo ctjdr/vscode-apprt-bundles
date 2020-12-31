1cb1558554ec4f79f44cd94cc47fa4cf98a61ba4
Jan Drewnak <j.drewnak@conterra.de>
2020-12-31 11:51:45 +0100

    Create empty range for undefined sections

--- src/features/Range.ts
+++ src/features/Range.ts
@@ -1,7 +1,10 @@
 import { Position, Range } from "vscode";
 import { Section } from "../bundles/ManifestDocument";
 
-export function rangeOfSection(section: Section): Range {
+export function rangeOfSection(section: Section | undefined): Range {
+    if (!section) {
+        return new Range(0,0,0,0);
+    }
     return new Range(
         new Position(section.start.line, section.start.col),
         new Position(section.end.line, section.end.col)
