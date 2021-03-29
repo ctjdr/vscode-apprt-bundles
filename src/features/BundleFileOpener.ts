54f8e7917a900ca40ae7d0f92aba3572c1dbe4a2
Jan Drewnak <j.drewnak@conterra.de>
2021-03-29 15:52:38 +0200

    Remove duplication, add comments

--- src/features/BundleFileOpener.ts
+++ src/features/BundleFileOpener.ts
@@ -2,28 +2,31 @@ import * as vscode from "vscode";
 import * as path from "path";
 import * as fs from "fs";
 
+
+/**
+ * Registers commands to to instantly open one the follwoing files of the current bundle.
+ *    
+ *   - ` manifest.json`
+ *   - `README.md`
+ * 
+ * The _current bundle_ is determined by the file of the active editor.
+ *  
+ */
 export class BundleFileOpener {
 
     register(): vscode.Disposable[] {
         return [
-            vscode.commands.registerCommand("apprtbundles.bundles.open.manifest",
-                () => {
-                    const bundleData = this.findCurrentBundleFolder();
-                    if (bundleData) {
-                        vscode.window.showTextDocument(vscode.Uri.file(path.join(bundleData, "manifest.json")));
-                    }
-                }
-                ),
-            vscode.commands.registerCommand("apprtbundles.bundles.open.readme",
-                () => {
-                    const bundleData = this.findCurrentBundleFolder();
-                    if (bundleData) {
-                        vscode.window.showTextDocument(vscode.Uri.file(path.join(bundleData, "README.md")));
-                    }
-                }
-                )
+            vscode.commands.registerCommand("apprtbundles.bundles.open.manifest", () => this.showBundleFile("manifest.json")),
+            vscode.commands.registerCommand("apprtbundles.bundles.open.readme", () => this.showBundleFile("README.md"))
             ];
         }
+
+    private showBundleFile(relativeFileName: string) {
+        const bundleData = this.findCurrentBundleFolder();
+        if (bundleData) {
+            vscode.window.showTextDocument(vscode.Uri.file(path.join(bundleData, relativeFileName)));
+        }
+    }
         
     private findCurrentBundleFolder() {
         if (!vscode.window.activeTextEditor) {
